03d2c62b283f883876619b0250a3888e948e08b2
Jonny Saykosy <josayko@pm.me>
2024-12-20 12:39:35 +0100

    feat: add memory to the RAG application

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import os
-from typing import Any
+import time
+from datetime import datetime
+from typing import Any, List
 
 from dotenv import load_dotenv
 from fireworks.client import Fireworks
@@ -23,6 +25,8 @@ if pong["ok"] == 1:
     print("MongoDB connection successful")
 
 COLLECTION_NAME = "knowledge_base"
+history_collection = mongodb_driver.client[mongodb_driver.db_name]["chat_history"]
+history_collection.create_index("session_id")
 
 
 # Define a function to create the user prompt for our RAG application
@@ -118,6 +122,99 @@ def generate_answer_2(user_query: str) -> None:
             print(chunk.choices[0].delta.content, end="")
 
 
+def store_chat_message(session_id: str, role: str, content: str) -> None:
+    """
+    Store a chat message in a MongoDB collection.
+
+    Args:
+        session_id (str): Session ID of the message.
+        role (str): Role for the message. One of `system`, `user` or `assistant`.
+        content (str): Content of the message.
+    """
+    # Create a message object with `session_id`, `role`, `content` and `timestamp` fields
+    # `timestamp` should be set the current timestamp
+    message = {
+        "session_id": session_id,
+        "role": role,
+        "content": content,
+        "timestamp": datetime.now(),
+    }
+    # Insert the `message` into the `history_collection` collection
+    history_collection.insert_one(message)
+
+
+def retrieve_session_history(session_id: str) -> List:
+    """
+    Retrieve chat message history for a particular session.
+
+    Args:
+        session_id (str): Session ID to retrieve chat message history for.
+
+    Returns:
+        List: List of chat messages.
+    """
+    # Query the `history_collection` collection for documents where the "session_id" field has the value of the input `session_id`
+    # Sort the results in increasing order of the values in `timestamp` field
+    cursor = history_collection.find({"session_id": session_id}).sort("timestamp", 1)
+
+    if cursor:
+        # Iterate through the cursor and extract the `role` and `content` field from each entry
+        # Then format each entry as: {"role": <role_value>, "content": <content_value>}
+        messages = [{"role": msg["role"], "content": msg["content"]} for msg in cursor]
+    else:
+        # If cursor is empty, return an empty list
+        messages = []
+
+    return messages
+
+
+def generate_answer_3(session_id: str, user_query: str) -> None:
+    """
+    Generate an answer to the user's query taking chat history into account.
+
+    Args:
+        session_id (str): Session ID to retrieve chat history for.
+        user_query (str): The user's query string.
+    """
+    # Initialize list of messages to pass to the chat completion model
+    messages = []
+
+    # Retrieve documents relevant to the user query and convert them to a single string
+    context = mongodb_driver.vector_search(COLLECTION_NAME, user_query)
+    context = "\n\n".join([d.get("body", "") for d in context])
+    # Create a system prompt containing the retrieved context
+    system_message = {
+        "role": "system",
+        "content": f"Answer the question based only on the following context. If the context is empty, say I DON'T KNOW\n\nContext:\n{context}",
+    }
+    # Append the system prompt to the `messages` list
+    messages.append(system_message)
+
+    # Use the `retrieve_session_history` function to retrieve message history from MongoDB for the session ID `session_id`
+    # And add all messages in the message history to the `messages` list
+    message_history = retrieve_session_history(session_id)
+    messages.extend(message_history)
+
+    # Format the user message in the format {"role": <role_value>, "content": <content_value>}
+    # The role value for user messages must be "user"
+    # And append the user message to the `messages` list
+    user_message = {"role": "user", "content": user_query}
+    messages.append(user_message)
+
+    # Call the chat completions API
+    response = fw_client.chat.completions.create(model=model, messages=messages)
+
+    # Extract the answer from the API response
+    answer = response.choices[0].message.content  # type: ignore
+
+    # Use the `store_chat_message` function to store the user message and also the generated answer in the message history collection
+    # The role value for user messages is "user", and "assistant" for the generated answer
+    store_chat_message(session_id, "user", user_query)
+    store_chat_message(session_id, "assistant", answer)
+
+    print(answer)
+
+
 if __name__ == "__main__":
     # Run the `generate_answer` function with a user query
     # user_query_1 = "What is MongoDB Atlas Search?"
@@ -145,6 +242,20 @@ if __name__ == "__main__":
     # print(user_query_5)
     # time.sleep(3)
     # print(answer_5)
-    user_query_6 = "What is Atlas Search and how Atlas Search maps documents?"
-    print(user_query_6)
-    generate_answer_2(user_query_6)
+    # user_query_6 = "What is Atlas Search and how Atlas Search maps documents?"
+    # print(user_query_6)
+    # generate_answer_2(user_query_6)
+
+    print("What are triggers in MongoDB Atlas?")
+    generate_answer_3(
+        session_id="3",
+        user_query="What are triggers in MongoDB Atlas?",
+    )
+
+    time.sleep(3)
+
+    print("What did I just ask you?")
+    generate_answer_3(
+        session_id="3",
+        user_query="What did I just ask you?",
+    )
